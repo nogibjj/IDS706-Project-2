a8e033f736122458b80052ebb4ec3bb4830d78f1
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-10-09 09:29:21 +0000

    update batch sent

--- batch_sent.sh
+++ batch_sent.sh
@@ -11,11 +11,6 @@ if [ ! -d $OUTPUT_FILE ]; then
     touch $OUTPUT_FILE
 fi
 
-# go through each line in the input file and print it
-while read line; do
-    # ret=$(curl http://localhost:7860/api/analytics --data-raw "{\"data\":[$line]}" -X POST -H 'Content-Type: application/json')
-    python inference.py "$line" $OUTPUT_FILE
-    # python write_output.py "$ret" $OUTPUT_FILE "$line"
-done < $INPUT_FILE
+python inference.py $INPUT_FILE $OUTPUT_FILE
 
 cat $OUTPUT_FILE
